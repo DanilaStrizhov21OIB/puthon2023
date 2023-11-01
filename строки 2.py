81de4cbb4930cec89d7cc05408d5369941c4ab9c
Данила 31-оиб <67444202+DanilaStrizhov21OIB@users.noreply.github.com>
2023-11-01 11:29:43 +0300

    Update строки 2.py

--- строки 2.py
+++ строки 2.py
@@ -1,5 +1,8 @@
-mystring=str(input())
-
-err=' '.join(mystring.split())
-
-print(err)
+text = input("Введите строку: ")
+cleaned_text = ""
+for i in range(len(text)):
+    if text[i] == " ":
+        if i+1 < len(text) and text[i+1] == " ":
+            continue
+    cleaned_text += text[i]
+print("Очищенная строка:", cleaned_text)

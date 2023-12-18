6796bce2f91d569862c8a3f5d4912ee7cbff8242
Данила 31-оиб <67444202+DanilaStrizhov21OIB@users.noreply.github.com>
2023-12-18 19:39:03 +0300

    Add files via upload

---
+++ ООП 2.py
@@ -0,0 +1,16 @@
+class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+    def seating_capacity(self, capacity):
+        return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+
+class Autobus(Transport):
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+    def seating_capacity(self, capacity=50):
+        return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+
+renault_logan_bus = Autobus("Renault Logan", 100, 50000)
+print(renault_logan_bus.seating_capacity())
